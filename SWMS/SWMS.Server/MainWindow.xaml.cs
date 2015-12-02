29585758faabe072ec5ade6c8ad7c7d239c949c1
Roman Rudyak <roma.rudyak@gmail.com>
2015-12-02 15:57:28 +0200

    Add basic lay out

--- SWMS/SWMS.Server/MainWindow.xaml.cs
+++ SWMS/SWMS.Server/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Kinect;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,88 @@ namespace SWMS.Server
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindow_Loaded;
+            this.Closed += MainWindow_Closed;
         }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            _sensor = KinectSensor.GetDefault();
+
+            _sensor.IsAvailableChanged += IsAvailableChanged;
+
+            FrameDescription colorFrameDescription = _sensor.ColorFrameSource.CreateFrameDescription(ColorImageFormat.Bgra);
+
+            _colorBitmap = new WriteableBitmap(colorFrameDescription.Width, colorFrameDescription.Height, 96.0, 96.0, PixelFormats.Bgr32, null);
+
+            KinectViewSpace.Source = _colorBitmap;
+
+            _multiReader = _sensor.OpenMultiSourceFrameReader(FrameSourceTypes.Color | FrameSourceTypes.Depth | FrameSourceTypes.Body);
+
+            _multiReader.MultiSourceFrameArrived += FrameArrived;
+
+            UpdateKinectStaus(_sensor.IsAvailable);
+        }
+
+        private void FrameArrived(object sender, MultiSourceFrameArrivedEventArgs e)
+        {
+            var multiframe = e.FrameReference.AcquireFrame();
+            using (ColorFrame colorFrame = multiframe.ColorFrameReference.AcquireFrame())
+            {
+                if (colorFrame != null)
+                {
+                    FrameDescription colorFrameDescription = colorFrame.FrameDescription;
+
+                    using (KinectBuffer colorBuffer = colorFrame.LockRawImageBuffer())
+                    {
+                        this._colorBitmap.Lock();
+
+                        // verify data and write the new color frame data to the display bitmap
+                        if ((colorFrameDescription.Width == this._colorBitmap.PixelWidth) && (colorFrameDescription.Height == this._colorBitmap.PixelHeight))
+                        {
+                            colorFrame.CopyConvertedFrameDataToIntPtr(
+                                this._colorBitmap.BackBuffer,
+                                (uint)(colorFrameDescription.Width * colorFrameDescription.Height * 4),
+                                ColorImageFormat.Bgra);
+
+                            this._colorBitmap.AddDirtyRect(new Int32Rect(0, 0, this._colorBitmap.PixelWidth, this._colorBitmap.PixelHeight));
+                        }
+
+                        this._colorBitmap.Unlock();
+                    }
+                }
+            }
+        }
+
+        private void IsAvailableChanged(object sender, IsAvailableChangedEventArgs e)
+        {
+            UpdateKinectStaus(e.IsAvailable);
+        }
+
+        private void UpdateKinectStaus(Boolean isAvailable)
+        {
+            KinectStatus.Content = isAvailable
+                ? "Connected"
+                : "Disconnected";
+        }
+
+        private void MainWindow_Closed(object sender, EventArgs e)
+        {
+            if (_multiReader != null)
+            {
+                _multiReader.Dispose();
+                _multiReader = null;
+            }
+            if (_sensor != null)
+            {
+                _sensor.Close();
+                _sensor = null;
+            }
+        }
+
+        private WriteableBitmap _colorBitmap;
+        private KinectSensor _sensor;
+        private MultiSourceFrameReader _multiReader;
+        private CoordinateMapper _coordinateMapper;
     }
 }
